7b57293a916b8b294c3094ccce0b9cffc9125483
bahalla <bahalla1997@gmail.com>
2019-12-16 15:05:06 +0100

    create card and guessCount compoenents and add events

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import GuessCount from "./GuessCount";
+import Card from "./Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  handleCardClick(card) {
+    console.log(card + ' clicked');
+  }
+  render() {
+    return (
+        <div className="memory">
+          <GuessCount guesses={0}></GuessCount>
+          <Card card="😀" feedback="hidden" onClick={this.handleCardClick}/>
+          <Card card="💖🎉" feedback="justMatched" onClick={this.handleCardClick} />
+          <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+          <Card card="🎩🎩" feedback="visible" onClick={this.handleCardClick} />
+          <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        </div>
+        )
+  }
 }
 
-export default App;
+export default App
